7d27c5da4c14a06233139095fafd2125f6aa9ea9
JonathanBY15 <bariscanyaprak@gmail.com>
2024-10-16 19:51:18 -0400

    add GamePriceCard

---
+++ src/components/GamePriceCard/GamePriceCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './GamePriceCard.css'; // Import the CSS for styling
+
+const GamePriceCard = ({ gameImage, gameTitle, stores }) => {
+  return (
+    <div className="game-price-card">
+      {/* Game image */}
+      <img src={gameImage} alt={gameTitle} className="game-image" />
+
+      {/* Game Title */}
+      <h3 className="game-title">{gameTitle}</h3>
+
+      {/* Store/Price pairs */}
+      <div className="store-price-list">
+        {stores.map((store, index) => (
+          <div className="store-price-item" key={index}>
+            <span className="store-name">{store.name}</span>
+            <span className="price">{`$${store.price}`}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GamePriceCard;
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -5,7 +5,42 @@ import ColoredTitle from '../../components/ColoredTitle/ColoredTitle';
 import ColoredButton from '../../components/ColoredButton/ColoredButton';
 import LogoSlider from '../../components/LogoSlider/LogoSlider';
 import SubTitle from '../../components/SubTitle/SubTitle';
+import GamePriceCard from '../../components/GamePriceCard/GamePriceCard';
 import './LandingPage.css';
+
+
+const stores = [
+  { name: 'Steam', price: '29.99' },
+  { name: 'Epic Games', price: '27.99' },
+  { name: 'Gog', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+];
+
+const rdr_stores = [
+  { name: 'Steam', price: '29.99' },
+  { name: 'Epic Games', price: '27.99' },
+  { name: 'Gog', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+];
+
+const detroit_stores = [
+  { name: 'Steam', price: '29.99' },
+  { name: 'Epic Games', price: '27.99' },
+  { name: 'Gog', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+  { name: 'Origin', price: '30.99' },
+];
 
 const LandingPage = () => {
   return (
@@ -35,6 +70,40 @@ const LandingPage = () => {
     </div>
 
     <LogoSlider />
+
+    <SubTitle text="Popular Games"/>
+
+    <div className="game-price-card-container">
+      <div>
+        <GamePriceCard 
+          gameImage="https://shared.cloudflare.steamstatic.com//store_item_assets//steam//apps//1174180//capsule_sm_120.jpg?t=1720558643" 
+          gameTitle="Red Dead Redemption 2" 
+          stores={rdr_stores} 
+        />
+      </div>
+      <div>
+        <GamePriceCard 
+          gameImage="https:\/\/shared.cloudflare.steamstatic.com\/store_item_assets\/steam\/apps\/213330\/capsule_sm_120.jpg?t=1726531114" 
+          gameTitle="LEGO Batman 2: DC Super Heroes" 
+          stores={stores} 
+        />
+      </div>
+      <div>
+        <GamePriceCard 
+          gameImage="https://shared.cloudflare.steamstatic.com//store_item_assets//steam//apps//1091500//capsule_sm_120.jpg?t=1726188854" 
+          gameTitle="Cyberpunk 2077" 
+          stores={stores} 
+        />
+      </div>
+      <div>
+        <GamePriceCard 
+          gameImage="https:\/\/shared.cloudflare.steamstatic.com\/store_item_assets\/steam\/apps\/1222140\/capsule_sm_120.jpg?t=1667468479" 
+          gameTitle="Detroit: Become Human" 
+          stores={detroit_stores} 
+        />
+      </div>
+      
+    </div>    
 
   </div>
 
