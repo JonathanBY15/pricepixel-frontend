7fb009e534ca7580c3364eb42bc40337c454c472
JonathanBY15 <bariscanyaprak@gmail.com>
2024-10-18 20:01:58 -0400

    add style to NavBar links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,10 @@
-// // src/components/NavBar.js
 // import React from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
+// import { Link, useNavigate, useLocation } from 'react-router-dom';
 // import './NavBar.css'; // Optional: for styling
 
 // const NavBar = () => {
 //   const navigate = useNavigate(); // For programmatic navigation
+//   const location = useLocation(); // To get current route
 //   const isLoggedIn = !!localStorage.getItem('token'); // Check if token exists in localStorage
 
 //   const handleLogout = () => {
@@ -12,33 +12,40 @@
 //     navigate('/login'); // Redirect to login page after logout
 //   };
 
+//   const handleLogoClick = (event) => {
+//     if (location.pathname === '/') {
+//       event.preventDefault(); // Prevent default navigation if on the same page
+//       window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top of the page smoothly
+//     }
+//   };
+
 //   return (
 //     <nav className="navbar">
 //       <div className="navbar-container">
-//         <Link to="/" className="navbar-logo">
+//         <Link to="/" className="navbar-logo" onClick={handleLogoClick}>
 //           <h1>PricePixel</h1>
 //         </Link>
 //         <ul className="navbar-menu">
-//             <li>
-//               <Link to="/" className="navbar-link">Home</Link>
-//             </li>
+//           <li>
+//             <Link to="/" className="navbar-link" onClick={handleLogoClick}><i class="fa-solid fa-house"></i> Home</Link>
+//           </li>
 //           {isLoggedIn ? (
 //             <li>
 //               <Link 
-//                 to="/login" // This sets the destination for the link
+//                 to="/login"
 //                 className="navbar-link"
-//                 onClick={handleLogout} // Call handleLogout on click
+//                 onClick={handleLogout}
 //               >
-//                 Sign Out
+//                 <i class="fa-solid fa-right-from-bracket"></i> Log Out
 //               </Link>
 //             </li>
 //           ) : (
 //             <>
 //               <li>
-//                 <Link to="/login" className="navbar-link">Login</Link>
+//                 <Link to="/login" className="navbar-link"><i class="fa-solid fa-right-to-bracket"></i> Login</Link>
 //               </li>
 //               <li>
-//                 <Link to="/signup" className="navbar-link">Signup</Link>
+//                 <Link to="/signup" className="navbar-link"><i class="fa-solid fa-user-plus"></i> Signup</Link>
 //               </li>
 //             </>
 //           )}
@@ -49,7 +56,6 @@
 // };
 
 // export default NavBar;
-
 
 
 
@@ -82,7 +88,13 @@ const NavBar = () => {
         </Link>
         <ul className="navbar-menu">
           <li>
-            <Link to="/" className="navbar-link" onClick={handleLogoClick}><i class="fa-solid fa-house"></i> Home</Link>
+            <Link 
+              to="/" 
+              className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`} 
+              onClick={handleLogoClick}
+            >
+              <i className="fa-solid fa-house"></i> Home
+            </Link>
           </li>
           {isLoggedIn ? (
             <li>
@@ -91,16 +103,26 @@ const NavBar = () => {
                 className="navbar-link"
                 onClick={handleLogout}
               >
-                <i class="fa-solid fa-right-from-bracket"></i> Log Out
+                <i className="fa-solid fa-right-from-bracket"></i> Log Out
               </Link>
             </li>
           ) : (
             <>
               <li>
-                <Link to="/login" className="navbar-link"><i class="fa-solid fa-right-to-bracket"></i> Login</Link>
+                <Link 
+                  to="/login" 
+                  className={`navbar-link ${location.pathname === '/login' ? 'active' : ''}`}
+                >
+                  <i className="fa-solid fa-right-to-bracket"></i> Login
+                </Link>
               </li>
               <li>
-                <Link to="/signup" className="navbar-link"><i class="fa-solid fa-user-plus"></i> Signup</Link>
+                <Link 
+                  to="/signup" 
+                  className={`navbar-link ${location.pathname === '/signup' ? 'active' : ''}`}
+                >
+                  <i className="fa-solid fa-user-plus"></i> Signup
+                </Link>
               </li>
             </>
           )}
